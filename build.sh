89e8572f30d73bad844bf7ff41828922e083ae29
James Edmonston <james@ethercreative.co.uk>
2021-12-17 14:50:34 +0000

    Build MySQL images first

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
-docker build --build-arg WITH_POSTGRES=true -t ethercreative/craft-php-fpm-alpine:postgres_php8 .;
-docker push ethercreative/craft-php-fpm-alpine:postgres_php8;
-
-docker build --build-arg WITH_POSTGRES=true --build-arg WITH_XML=true -t ethercreative/craft-php-fpm-alpine:postgres-xml_php8 .;
-docker push ethercreative/craft-php-fpm-alpine:postgres-xml_php8;
-
 docker build --build-arg WITH_MYSQL=true -t ethercreative/craft-php-fpm-alpine:mysql_php8 .;
 docker push ethercreative/craft-php-fpm-alpine:mysql_php8;
 
 docker build --build-arg WITH_MYSQL=true --build-arg WITH_XML=true -t ethercreative/craft-php-fpm-alpine:mysql-xml_php8 .;
 docker push ethercreative/craft-php-fpm-alpine:mysql-xml_php8;
+
+docker build --build-arg WITH_POSTGRES=true -t ethercreative/craft-php-fpm-alpine:postgres_php8 .;
+docker push ethercreative/craft-php-fpm-alpine:postgres_php8;
+
+docker build --build-arg WITH_POSTGRES=true --build-arg WITH_XML=true -t ethercreative/craft-php-fpm-alpine:postgres-xml_php8 .;
+docker push ethercreative/craft-php-fpm-alpine:postgres-xml_php8;
