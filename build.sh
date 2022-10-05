a9a3532b65dc1624391079f710453b291efe739a
James Edmonston <james@ethercreative.co.uk>
2022-10-05 12:19:56 +0100

    Fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build --build-arg -t ethercreative/craft-php-fpm-alpine:mysql_php8 .;
+docker build -t ethercreative/craft-php-fpm-alpine:mysql_php8 .;
 docker push ethercreative/craft-php-fpm-alpine:mysql_php8;
 
-docker build --build-arg --build-arg WITH_XML=true -t ethercreative/craft-php-fpm-alpine:mysql-xml_php8 .;
+docker build --build-arg WITH_XML=true -t ethercreative/craft-php-fpm-alpine:mysql-xml_php8 .;
 docker push ethercreative/craft-php-fpm-alpine:mysql-xml_php8;
